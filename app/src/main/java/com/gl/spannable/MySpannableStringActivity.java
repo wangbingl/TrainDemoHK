0c158bb4a8d030e9492608f62ad3ed4e3072499f
wang_bin <wang_bin@gionee.com>
2017-07-29 17:32:00 +0800

    第4课  spannable str

--- app/src/main/java/com/gl/spannable/MySpannableStringActivity.java
+++ app/src/main/java/com/gl/spannable/MySpannableStringActivity.java
@@ -2,36 +2,139 @@ package com.gl.spannable;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.AbsoluteSizeSpan;
 import android.text.style.ForegroundColorSpan;
-import android.text.style.TextAppearanceSpan;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import com.gl.traindemohk.R;
 
 /**
  * Created by 王斌 on 2017/7/28.
+ * http://www.2cto.com/kf/201610/553312_2.html
  */
 
 public class MySpannableStringActivity extends AppCompatActivity {
     private TextView mTextView;
+
+
+    int titleFontSize;/*= getResources().getDimensionPixelSize(R.dimen.spannable_string_titleFontSize); // 标题字号*/
+    int subtitleSize; /*= getResources().getDimensionPixelSize(R.dimen.spannable_string_subtitleSize); //副标题字号*/
+    int titleColor;/*= Color.parseColor("#FF666666"); // 标题颜色*/
+    int subtitleColor;/*= Color.parseColor("#FF888888"); // 副标题颜色*/
+    int spanColor;/*= Color.parseColor("#FFFF9000");*/
+    int transColor;/*= getResources().getColor(android.R.color.transparent); // 透明色*/
+
+
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_spannable_string);
         mTextView = (TextView) findViewById(R.id.text_spannable);
 
-        SpannableStringBuilder builder1 = new SpannableStringBuilder(getString(R.string.spannable_string_text1));
-        builder1.setSpan(new TextAppearanceSpan(this, android.R.style.TextAppearance_Medium),0,builder1.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
 
-        SpannableStringBuilder builder2 = new SpannableStringBuilder(getString(R.string.spannable_string_text2));
-        builder2.setSpan(new ForegroundColorSpan(Color.BLUE),builder2.toString().indexOf("^"),builder2.toString().indexOf("&"), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
-        mTextView.append(builder1.toString());
-        mTextView.append(builder2.toString());
+        titleFontSize = getResources().getDimensionPixelSize(R.dimen.spannable_string_titleFontSize); // 标题字号
+        subtitleSize = getResources().getDimensionPixelSize(R.dimen.spannable_string_subtitleSize); //副标题字号
+        titleColor = Color.parseColor("#FF666666"); // 标题颜色
+        subtitleColor = Color.parseColor("#FF888888"); // 副标题颜色
+        spanColor = Color.parseColor("#FFFF9000");
+        transColor = getResources().getColor(android.R.color.transparent); // 透明色
 
+
+        buildSpan();
+        mTextView.setVisibility(View.VISIBLE);
+    }
+
+
+    private void buildSpan() {
+
+        SpannableStringBuilder builder = new SpannableStringBuilder();
+
+
+        SpannableStringBuilder builder1 = buildSpannableString(R.string.spannable_string_text1, titleFontSize, titleColor, spanColor);
+
+        SpannableStringBuilder builder2 = buildSpannableString(R.string.spannable_string_text2, subtitleSize, subtitleColor, spanColor);
+        setTextTextAlignment(builder2, titleFontSize, transColor);
+
+        SpannableStringBuilder builder3 = buildSpannableString(R.string.spannable_string_text3, titleFontSize, titleColor, spanColor);
+
+        SpannableStringBuilder builder4 = buildSpannableString(R.string.spannable_string_text4, subtitleSize, subtitleColor, spanColor);
+//        setTextTextAlignment(builder4,titleFontSize,transColor);
+        // 设置文本颜色
+
+
+        builder.append(builder1);
+        builder.append(builder2);
+        builder.append(builder3);
+        builder.append(builder4);
+
+
+        mTextView.setText(builder);
+
+    }
+
+    private SpannableStringBuilder buildSpannableString(int resId, int fontSize, int textColor, int spanColor) {
+        SpannableStringBuilder builder = new SpannableStringBuilder(getString(resId));
+
+        setTextColor(builder, textColor);
+        if (resId == R.string.spannable_string_text4) {
+            builder.setSpan(new ForegroundColorSpan(transColor), 0, 3, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        }
+
+        setTextFontSize(builder, fontSize);
+
+        if (resId == R.string.spannable_string_text4) {
+            builder.setSpan(new AbsoluteSizeSpan(titleFontSize, false), 0, 3, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        }
+
+        setSpanColor(builder, spanColor);
+
+
+        return builder;
+    }
+
+
+    private void setTextColor(SpannableStringBuilder builder, int color) {
+        builder.setSpan(new ForegroundColorSpan(color), 0, builder.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+    }
+
+    private void setTextFontSize(SpannableStringBuilder builder, int size) {
+        builder.setSpan(new AbsoluteSizeSpan(size, false), 0, builder.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+    }
+
+    /**
+     * 设置高亮字符串颜色
+     */
+//    private void setSpanColor(SpannableStringBuilder builder,int color){}
+    private void setSpanColor(SpannableStringBuilder builder, int color) {
+        String text = builder.toString();
+
+        int start = text.indexOf("^");
+        int end = text.indexOf("$");
+
+        while (start >= 0 && end >= 0) {
+            Log.i("wb", "[setSpanColor] start:" + start + " | end:" + end);
+            builder.setSpan(new ForegroundColorSpan(color), start, end, Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
+
+            builder.replace(start, start + 1, "");
+            builder.replace(end - 1, end, "");
+
+            text = builder.toString();
+            Log.i("wb", "[setSpanColor] after ed text:" + text);
+            start = text.indexOf("^");
+            end = text.indexOf("$");
+        }
+    }
+
+    private void setTextTextAlignment(SpannableStringBuilder builder, int size, int color) {
+        builder.setSpan(new AbsoluteSizeSpan(size, false), 0, 3, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        builder.setSpan(new ForegroundColorSpan(color), 0, 3, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
     }
 }
--- app/src/main/java/com/gl/traindemohk/MyPracticeFragment.java
+++ app/src/main/java/com/gl/traindemohk/MyPracticeFragment.java
@@ -9,13 +9,14 @@ import com.gl.spannable.MySpannableStringActivity;
 
 /**
  * Created by wb on 17-7-26.
+ *
  */
 
 public class MyPracticeFragment extends BaseFragment {
     private static final String TAG = MyPracticeFragment.class.getSimpleName();
     @Override
     String[] setData() {
-        return new String[]{"1.动画", "2.二维码", "3.高德定位及地图", "4.微信登录、支付、公众号", "5.支付宝支付", "6.热更新、热修复技术","7.SpannableString"};
+        return new String[]{"1.动画", "2.二维码", "3.高德定位及地图", "4.微信登录、支付、公众号", "5.支付宝支付", "6.热更新、热修复技术", "7.SpannableString", "8.SQLite触发器", "9.SIM卡联系人导入技术"};
     }
 
     @Override
